d096b9a78c74af21ba4a626ab6a3514f12407f5c
dodaro <dodaro@mat.unical.it>
2023-02-09 13:57:19 +0100

    Cambiata annotazione.

--- Authentication/SpringSecurityOpenIDConnect/src/main/java/it/unical/demacs/informatica/springsecurityopenidconnect/OAuth2ClientSecurityConfig.java
+++ Authentication/SpringSecurityOpenIDConnect/src/main/java/it/unical/demacs/informatica/springsecurityopenidconnect/OAuth2ClientSecurityConfig.java
@@ -2,12 +2,12 @@ package it.unical.demacs.informatica.springsecurityopenidconnect;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class OAuth2ClientSecurityConfig {
 
     @Bean
